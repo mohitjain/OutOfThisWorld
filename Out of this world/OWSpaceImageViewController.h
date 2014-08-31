92c0917771edbf1d2180c56151f74176214fca09
Mohit Jain <jain.mohit27@gmail.com>
2014-08-31 11:11:44 +0530

    Showing list of all planets and a static image on the scrollview

---
+++ Out of this world/OWSpaceImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWSpaceImageViewController.h
+//  Out of this world
+//
+//  Created by Mohit Jain on 31/08/14.
+//  Copyright (c) 2014 CodeBeerStartups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceImageViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *imageView;
+
+@end
